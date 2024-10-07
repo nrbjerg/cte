db76236847bf91eaf59612ad2be2ab0df47a4d3f
Martin Sig Nørbjerg <mart547n@outlook.dk>
2024-10-07 18:27:57 +0200

    implemented correct dijsktra, but did not fix bugs in reversed dijstra

--- source/core/dijskstra.py
+++ source/core/dijskstra.py
@@ -6,11 +6,44 @@ from classes.route import Route
 from dataclasses import dataclass, field
 import numpy as np
 
+def dijkstra(nodes: List[Node], start: Node, end: Node) -> Route:
+    """Computes the shortest route from the start to the end node."""
+    if start == end:
+        return Route([start])
+    
+    # 1. + 2. Mark all nodes as unvisited and assign the current shortest known distance
+    smallest_distances = {node.node_id: 0 if node == start else np.inf for node in nodes}
+    shortest_routes = {start.node_id: Route([start])}
+
+    unvisited = set([node.node_id for node in nodes])
+    while len(unvisited) > 0:
+        # Pick the node with the smallest current distance.
+        current_id = min(unvisited, key = lambda node_id: smallest_distances[node_id])
+        node = nodes[current_id]
+        
+        for adjacent_node in node.adjacent_nodes:
+            if adjacent_node.node_id not in unvisited:
+                continue
+        
+            dist = np.linalg.norm(node.pos - adjacent_node.pos)
+            if ((adjacent_node.node_id not in shortest_routes.keys()) or 
+                (smallest_distances[current_id] + dist < smallest_distances[adjacent_node.node_id])):
+
+                shortest_routes[adjacent_node.node_id] = Route(shortest_routes[current_id].nodes + [adjacent_node])
+                smallest_distances[adjacent_node.node_id] = smallest_distances[current_id] + dist 
+
+        unvisited.remove(current_id)
+
+    return shortest_routes[end.node_id]
+
 # TODO: Could probably be done more efficiently using a priority queue,  however the ones 
 #       that i have found, does not allow the user to update the priorities of the nodes.
-def reversed_dijkstra(nodes: List[Node], target_id: int = 1) -> Dict[int, Route]:
+def reversed_dijkstra(nodes: List[Node], target_id: int = -1) -> Dict[int, Route]:
     """Computes the shortest paths to the node with the target id for each node in the TOP instance."""
     # 1. + 2. Mark all nodes as unvisited and assign the current best known distance from the 
+    if target_id < 0:
+        target_id = len(nodes) - target_id
+
     smallest_costs = {node.node_id: 0 if node.node_id == target_id else np.inf for node in nodes}
     fastest_routes = {target_id: Route([nodes[target_id]], [])}
 
@@ -40,26 +73,24 @@ def reversed_dijkstra(nodes: List[Node], target_id: int = 1) -> Dict[int, Route]
 
 if __name__ == "__main__":
     nodes = [
-        Node(0, [], [], (0, 0), 1),
-        Node(1, [], [], (9, 9), 1),
-        Node(2, [], [], (3, 1), 1),
-        Node(3, [], [], (3, 3), 1),
-        Node(4, [], [], (0, 3), 1),
-        Node(5, [], [], (3, 5), 1),
-        Node(6, [], [], (6, 2), 1),
-        Node(7, [], [], (10, 3), 1),
+        Node(0, [], np.array([0, 0]), 1),
+        Node(1, [], np.array([9, 9]), 1),
+        Node(2, [], np.array([3, 1]), 1),
+        Node(3, [], np.array([3, 3]), 1),
+        Node(4, [], np.array([0, 3]), 1),
+        Node(5, [], np.array([3, 5]), 1),
+        Node(6, [], np.array([6, 2]), 1),
+        Node(7, [], np.array([10, 3]), 1),
     ]
 
-    nodes[0].adjacent_nodes = [nodes[2], nodes[4]]
-    nodes[1].adjacent_nodes = [nodes[5], nodes[7]]
-    nodes[2].adjacent_nodes = [nodes[0], nodes[3]]
-    nodes[3].adjacent_nodes = [nodes[2], nodes[4], nodes[6]]
-    nodes[4].adjacent_nodes = [nodes[0], nodes[3], nodes[5]]
-    nodes[5].adjacent_nodes = [nodes[4], nodes[1]]
-    nodes[6].adjacent_nodes = [nodes[3], nodes[7]]
-    nodes[7].adjacent_nodes = [nodes[6], nodes[1]]
+    nodes[0].adjacent_nodes = [nodes[2], nodes[4]] # a
+    nodes[1].adjacent_nodes = [nodes[5], nodes[7]] # b
+    nodes[2].adjacent_nodes = [nodes[0], nodes[3]] # c 
+    nodes[3].adjacent_nodes = [nodes[2], nodes[4], nodes[6]] # d
+    nodes[4].adjacent_nodes = [nodes[0], nodes[3], nodes[5]] # e
+    nodes[5].adjacent_nodes = [nodes[4], nodes[1]] # f
+    nodes[6].adjacent_nodes = [nodes[3], nodes[7]] # g
+    nodes[7].adjacent_nodes = [nodes[6], nodes[1]] # h
     
-    populate_costs(nodes)
-
-    routes = reversed_dijkstra(nodes)
-    print(routes)
+    route = dijkstra(nodes, nodes[0], nodes[7])
+    print(route)
