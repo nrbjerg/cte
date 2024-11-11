7e072aee23f408a245274f0cd32558f39ec396d9
Martin Sig Nørbjerg <mart547n@outlook.dk>
2024-11-11 10:01:48 +0100

    CPMHTOP instances created

--- scripts/test_dijskstra.py
+++ scripts/test_dijskstra.py
@@ -1,5 +1,5 @@
 from classes.problem_instances.top_instances import TOPInstance, load_TOP_instances
-from core.dijskstra import reversed_dijkstra
+from library.core.dijskstra import reversed_dijkstra
 
 initial_top_instance = load_TOP_instances(needs_plotting=True)[0]
 routes = reversed_dijkstra(initial_top_instance.nodes)
