5a2494704b4e61099f7dad7bbc73f86796a306c0
Martin Sig Nørbjerg <mart547n@outlook.dk>
2024-11-15 14:15:25 +0100

    Added support for plotting routes and computing their lengths

--- source/classes/problem_instances/cedopads_instances.py
+++ source/classes/problem_instances/cedopads_instances.py
@@ -1,10 +1,15 @@
+# %% 
 from __future__ import annotations
 from dataclasses import dataclass 
 from typing import List, Tuple
-from classes.data_types import Position, AngleInterval
+from classes.data_types import Position, AngleInterval, Angle, State
+import dubins
 import os 
 import numpy as np
 from matplotlib import pyplot as plt 
+from library.core.relaxed_dubins import compute_relaxed_dubins_path
+
+plt.rcParams['figure.figsize'] = [9, 9]
 
 class CEDOPADSNode:
     """Models a node in the DTOPADS problem."""
@@ -32,7 +37,6 @@ class CEDOPADSNode:
     @staticmethod
     def load_from_line(line: str) -> CEDOPADSNode:
         """Loads a CEDOPADS node from a line"""
-        print(line)
         position_and_score, angle_part = tuple(line.split(":"))
         x, y, score = map(float, position_and_score.split(" "))
         pos = np.array((x, y))
@@ -93,12 +97,9 @@ class CEDOPADS:
                 for i, size in enumerate(node_sizes):
                     nodes[i].size = size
 
-            for node in nodes:
-                print(node.intervals)
-
             return CEDOPADS(file_name[:-4], source, sink, nodes) 
 
-    def plot(self, r: float, show: bool = False):
+    def plot(self, sensing_radius: float, show: bool = False):
         """Displays a plot of the problem instance, and displays the coresponding angles."""
         plt.style.use("bmh")
 
@@ -107,12 +108,73 @@ class CEDOPADS:
 
         # Plots targets
         for node in self.nodes:
-            node.plot(r)
+            node.plot(sensing_radius)
         
         if show:
             plt.gca().set_aspect("equal", adjustable="box")
             plt.show()
+
+    def plot_with_route(self, route: List[Tuple[int, Angle]], sensing_radius: float, rho: float, color: str = "tab:orange"):
+        """Plots the CEDOPADS instance with a route"""
+        self.plot(sensing_radius)
+
+        if len(route) == 0:
+            raise ValueError("Got an empty route.")
+        
+        else:
+            q = self.get_states(route, sensing_radius)
+            for i, (k, _) in enumerate(route):
+                plt.scatter(*self.nodes[k].pos, c = color, s = self.nodes[k].size, zorder=2)
+                plt.scatter(*q[i].pos, marker="s", c = color, zorder=2)
+
+            # 1. Plot route from the source to q_1
+            k_1, psi_1 = route[0]
+            q_tilde = State(q[0].pos, (np.pi + psi_1) % (2 * np.pi))
+            first_path = compute_relaxed_dubins_path(q_tilde, self.source, rho, need_path=True)
+            first_path.plot(q_tilde, self.source, color = color)
+
+            # 2. Plot the dubins trajectories between q_i, q_i + 1
+            for i in range(len(q) - 1):
+                q0 = (q[i].pos[0], q[i].pos[1], q[i].angle)
+                q1 = (q[i + 1].pos[0], q[i + 1].pos[1], q[i + 1].angle)
+                configurations = np.array(dubins.path_sample(q0, q1, rho, 0.1)[0])
+                plt.plot(configurations[:, 0], configurations[:, 1], c = color)
+
+            # 3. Plot route from q_M to the sink 
+            final_path = compute_relaxed_dubins_path(q[-1], self.sink, rho, need_path=True)
+            final_path.plot(q[-1], self.sink, color = color)
+
+        plt.plot()
+    
+    def get_states(self, route: List[Tuple[int, Angle]], sensing_radius: float) -> List[State]:
+        """Returns a list of states corresponding to q_1, ..., q_M from the problem formulation."""
+        states = []
+        for k, psi in route:
+            pos = self.nodes[k].pos - sensing_radius * np.array([np.cos(psi), np.sin(psi)])
+            states.append(State(pos, psi))
+        return states
+
+
+    def compute_length_of_route(self, route: List[Tuple[int, Angle]], sensing_radius: float, rho: float) -> float:
+        """Computes the length of the route, for the given sensing radius and turning radius rho."""
+        q = self.get_states(route, sensing_radius) 
+        tups = [(q[i].pos[0], q[i].pos[1], q[i].angle) for i in range(len(q))]
+
+        q_tilde = State(q[0].pos, (np.pi + q[0].angle) % (2 * np.pi))
+
+        return (compute_relaxed_dubins_path(q_tilde, self.source, rho) + 
+                sum([dubins.shortest_path(tups[i], tups[i + 1], rho).path_length() for i in range(len(q) - 1)]) +
+                compute_relaxed_dubins_path(q[-1], self.sink, rho))
+
+
+    def is_route_feasable(self, route: List[Tuple[int, Angle]], sensing_radius: float, rho: float, tmax: float) -> bool:
+        """Checks if the route is feasable."""
+        return self.compute_length_of_route(route, sensing_radius, rho) <= tmax
             
 if __name__ == "__main__":
     problem = CEDOPADS.load_from_file("set_64_1_60.txt", needs_plotting = True)
-    problem.plot(r  = 1, show = True)
+    route = [(25, np.pi / 2), (6, -np.pi / 4), (20, np.pi)]
+    rho = 1.4
+    sensing_radius = 0.6
+    problem.plot_with_route(route, sensing_radius = sensing_radius, rho = rho) 
+    print(problem.compute_length_of_route(route, sensing_radius, rho))
--- source/library/core/relaxed_dubins.py
+++ source/library/core/relaxed_dubins.py
@@ -18,21 +18,27 @@ class Direction(IntEnum):
 class CSPath:
     """Models a path starting with a circular arc and ending with a straight path."""
     direction: Direction
-    p: Position
     radians_traversed_on_arc: Angle
     rho: float
     length: float
 
-    def plot(self, q: State, color: str = "tab:orange"): 
+    def plot(self, q: State, p: Position, color: str = "tab:orange"): 
         """Plots the path using matplotlib, using q for the inital state of the dubins vehicle."""
+        #plt.style.use("bmh")
+        #plt.scatter(*q.pos, marker = "s", c = color, zorder=2)
+        #plt.scatter(*p, marker = "s", c = color, zorder=2)
         # 1. Plot the arc
-        #rotation_angle = np.pi / 2 - q.angle
+        rotation_angle = np.pi / 2 - q.angle
+        rotation_matrix = np.array([[np.cos(rotation_angle), -np.sin(rotation_angle)], 
+                                    [np.sin(rotation_angle), np.cos(rotation_angle)]])
+
         if self.direction == Direction.RIGHT:
-            center = (self.rho, 0)
-            start, end = np.rad2deg(np.pi - self.radians_traversed_on_arc), np.rad2deg(np.pi)
+            center = rotation_matrix.T.dot(np.array([self.rho, 0])) + q.pos
+            start, end = np.rad2deg(q.angle + np.pi / 2 - self.radians_traversed_on_arc), np.rad2deg(q.angle + np.pi / 2)
+
         else:
-            center = (-self.rho, 0)
-            start, end = np.rad2deg(0), np.rad2deg(self.radians_traversed_on_arc)
+            center = rotation_matrix.T.dot(np.array([-self.rho, 0])) + q.pos
+            start, end = np.rad2deg(q.angle - np.pi / 2), np.rad2deg(q.angle - np.pi / 2 + self.radians_traversed_on_arc)
 
         arc = matplotlib.patches.Arc(center, 2 * self.rho, 2 * self.rho, theta1=start, theta2=end, edgecolor=color, lw = 2, zorder=2)
         plt.gca().add_patch(arc)
@@ -43,14 +49,13 @@ class CSPath:
         else:
             theta = self.radians_traversed_on_arc
 
-        inflection_point = center + self.rho * np.array([np.cos(theta), np.sin(theta)])
-        plt.plot([inflection_point[0], self.p[0]], [inflection_point[1], self.p[1]], c = color, zorder=2)
+        inflection_point = center + rotation_matrix.T.dot(self.rho * np.array([np.cos(theta), np.sin(theta)]))
+        plt.plot([inflection_point[0], p[0]], [inflection_point[1], p[1]], c = color, zorder=2)
     
     def __repr__(self) -> str:
         """Returns a string representation of the path."""
-        point = (round(float(self.p[0]), 2), round(float(self.p[1]), 2))
         path_type = "LS" if self.direction == Direction.LEFT else "RS"
-        return f"{path_type}({point}, radians: {self.radians_traversed_on_arc:.2f}, length: {self.length:.2f})"
+        return f"{path_type}(radians: {self.radians_traversed_on_arc:.2f}, length: {self.length:.2f})"
 
 @dataclass
 class CCPath:
@@ -60,57 +65,63 @@ class CCPath:
     rho: float
     length: float
 
-    def plot(self, q: State, color: str = "tab:orange"):
+    def plot(self, q: State, p: Position, color: str = "tab:orange"):
         """Plots the path using matplotlib, using q for the inital state of the dubins vehicle."""
-        # Plot the first arc.
+        #plt.style.use("bmh")
+        #plt.scatter(*q.pos, marker = "s", c = color, zorder=2)
+        #plt.scatter(*p, marker = "s", c = color, zorder=2)
+        rotation_angle = np.pi / 2 - q.angle
+        rotation_matrix = np.array([[np.cos(rotation_angle), -np.sin(rotation_angle)], 
+                                    [np.sin(rotation_angle), np.cos(rotation_angle)]])
+
         (u, v) = self.radians_traversed_on_arcs
+        # Plot the first arc.
         if self.directions[0] == Direction.RIGHT:
-            first_arc = matplotlib.patches.Arc((self.rho, 0), 2 * self.rho, 2 * self.rho,
-                                           theta1=np.rad2deg(np.pi - u), theta2=180, edgecolor=color, lw=2, zorder=2)
+            injuction_angle = 90 + np.rad2deg(q.angle - u)
+            first_center = rotation_matrix.T.dot(np.array([self.rho, 0])) + q.pos
+            first_arc = matplotlib.patches.Arc(first_center, 2 * self.rho, 2 * self.rho,
+                                           theta1=injuction_angle, theta2=np.rad2deg(q.angle) + 90, edgecolor=color, lw=2, zorder=2)
         else:
-            first_arc = matplotlib.patches.Arc((-self.rho, 0), 2 * self.rho, 2 * self.rho,
-                                           theta1=0, theta2=np.rad2deg(u), edgecolor=color, lw=2, zorder=2)
+            injuction_angle = np.rad2deg(q.angle + u) - 90
+            first_center = rotation_matrix.T.dot(np.array([-self.rho, 0])) + q.pos
+            first_arc = matplotlib.patches.Arc(first_center, 2 * self.rho, 2 * self.rho,
+                                           theta1=np.rad2deg(q.angle) - 90, theta2=injuction_angle, edgecolor=color, lw=2, zorder=2)
         plt.gca().add_patch(first_arc)
 
         # Plot the second arc.
         if self.directions[1] == Direction.LEFT:
-            second_center = (self.rho * (1 - 2 * np.cos(u)), 2 * self.rho * np.sin(u))
-            second_start = 180 + np.rad2deg(np.pi - u)
-            second_end = second_start + np.rad2deg(v) 
+            second_center = rotation_matrix.T.dot(np.array([self.rho * (1 - 2 * np.cos(u)), 2 * self.rho * np.sin(u)])) + q.pos
+            second_arc = matplotlib.patches.Arc(second_center, 2 * self.rho, 2 * self.rho,
+                                           theta1=injuction_angle + 180, theta2=injuction_angle + 180 + np.rad2deg(v), edgecolor=color, lw=2, zorder=2)
         else:
-            second_center = (self.rho * (2 * np.cos(u) - 1), 2 * self.rho * np.sin(u))
-            second_end = 180 + np.rad2deg(u)
-            second_start = second_end - np.rad2deg(v) 
+            second_center = rotation_matrix.T.dot(np.array([self.rho * (2 * np.cos(u) - 1), 2 * self.rho * np.sin(u)])) + q.pos
+            second_arc = matplotlib.patches.Arc(second_center, 2 * self.rho, 2 * self.rho,
+                                           theta1=180 - np.rad2deg(v) + injuction_angle, theta2=180 + injuction_angle, edgecolor=color, lw=2, zorder=2)
 
-        second_arc = matplotlib.patches.Arc(second_center, 2 * self.rho, 2 * self.rho,
-                                           theta1=second_start, theta2=second_end, edgecolor=color, lw=2, zorder=2)
         plt.gca().add_patch(second_arc)
-        #plt.scatter(self.rho * (2 * np.sin(u) - np.sin(u - v)), self.rho * (2 * np.cos(u) - np.cos(u - v) - 1))
 
     def __repr__(self) -> str:
         """Returns a string representation of the path."""
         path_type = "RL" if self.directions == (Direction.RIGHT, Direction.LEFT) else "LR"
         return f"{path_type}(radians: {self.radians_traversed_on_arcs[0]:.2f} and {self.radians_traversed_on_arcs[1]:.2f}, length: {self.length:.2f})"
 
-def compute_relaxed_dubins_path(q: State, p: Position, rho: float, plot: bool = False, need_path: bool = False) -> Union[CSPath, CCPath, float]:
+def compute_relaxed_dubins_path(q: State, p: Position, rho: float, plot: bool = False, verbose_plot: bool = False, need_path: bool = False) -> Union[CSPath, CCPath, float]:
     """Either computes the relaxed dubins path between the configuration q and the position p, or the length 
        of the relaxed dubins path between q and the position p, if need_path == False"""
     # 1. Transform the cordiante system so that q.pos is the origin and so that q.angle == pi / 2
     rotation_angle = np.pi / 2 - q.angle
-    rotation_matrix = np.array([[np.cos(rotation_angle), -np.sin(rotation_angle)], [np.sin(rotation_angle), np.cos(rotation_angle)]])
+    rotation_matrix = np.array([[np.cos(rotation_angle), -np.sin(rotation_angle)], 
+                                [np.sin(rotation_angle), np.cos(rotation_angle)]])
     p_prime = rotation_matrix.dot(p - q.pos) 
     # NOTE: Remember that this means that the q is located at 0, 0 with an initial heading of pi / 2
 
     c_l, c_r = np.array([-rho, 0]), np.array([rho, 0])
-    if plot:
-        plt.style.use("bmh")
-        plt.scatter(0, 0, marker = "s", c = "tab:orange", zorder=2)
-        plt.scatter(*p_prime, marker = "s", c = "tab:orange", zorder=2)
-        plt.gca().arrow(0, 0, 0, 1, color = "tab:orange", zorder=2, head_width=0.05)
 
+    if plot and verbose_plot:
+        plt.gca().arrow(q.pos[0], q.pos[1], np.cos(q.angle), np.sin(q.angle), color = "tab:orange", zorder=2, head_width=0.05)
         for color, center in zip(["tab:blue", "tab:green"], [c_l, c_r]): 
-            plt.scatter(*center, c = color)
-            plt.gca().add_patch(plt.Circle(center, rho, color = color, fill = False))
+            plt.scatter(*rotation_matrix.T.dot(center) + q.pos, c = color)
+            plt.gca().add_patch(plt.Circle(rotation_matrix.T.dot(center) + q.pos, rho, color = color, fill = False))
 
     if p_prime[0] >= 0: # Candidates are LS and RL
         distance_from_c_r = np.linalg.norm(p_prime - c_r)
@@ -141,7 +152,7 @@ def compute_relaxed_dubins_path(q: State, p: Position, rho: float, plot: bool = 
             if not need_path:
                 return length
             
-            path = CSPath(Direction.RIGHT, p_prime, b, rho, length)
+            path = CSPath(Direction.RIGHT, b, rho, length)
 
     else: 
         distance_from_c_l = np.linalg.norm(p_prime - c_l)
@@ -172,10 +183,10 @@ def compute_relaxed_dubins_path(q: State, p: Position, rho: float, plot: bool = 
             if not need_path:
                 return length
             
-            path = CSPath(Direction.LEFT, p_prime, b, rho, length)
+            path = CSPath(Direction.LEFT, b, rho, length)
 
     if plot:
-        path.plot(q)
+        path.plot(q, p)
         
     return path
 
@@ -184,8 +195,7 @@ def can_be_reached(q: State, p: Position, rho: float, remaining_travel_budget: f
     return compute_relaxed_dubins_path(q, p, rho, need_path=False) < remaining_travel_budget 
 
 if __name__ == "__main__":
-    angle = np.pi / 2 
-    q = State(np.array([0, 0]), angle)
-    p = np.array([1, 2])
-    print(compute_relaxed_dubins_path(q, p, 1, plot = True, need_path = True))
-    #print(f"Euclidian distance: {np.linalg.norm(q.pos - p)}")
+    angle = np.pi / 3 
+    q = State(np.array([-2.0, -5.6]), 4.71)
+    p = np.array([0, -7])
+    print(compute_relaxed_dubins_path(q, p, 1, plot = True, verbose_plot = False, need_path = True))
